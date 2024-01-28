5480ba793e845fa9ed47dad9c036823f581b0ac3
CamposDJoel <140183065+CamposDJoel@users.noreply.github.com>
2024-01-27 23:18:45 -0500

    Tools Class for Generic Static functionality methods

---
+++ YGO Card Collector 5/Tools.cs
@@ -0,0 +1,22 @@
+﻿//Joel Campos
+//1/27/2024
+//Tools Class
+
+using System;
+using System.Windows.Forms;
+
+namespace YGO_Card_Collector_5
+{
+    public static class Tools
+    {
+        public static void WaitNSeconds(double milliseconds)
+        {
+            if (milliseconds < 1) return;
+            DateTime _desired = DateTime.Now.AddMilliseconds(milliseconds);
+            while (DateTime.Now < _desired)
+            {
+                Application.DoEvents();
+            }
+        }
+    }
+}
