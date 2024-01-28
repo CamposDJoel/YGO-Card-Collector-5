c2bdfd1427c6066862f01be4b8d68df0a87c70f7
CamposDJoel <140183065+CamposDJoel@users.noreply.github.com>
2024-01-27 22:20:10 -0500

    MasterCard and SetCard Objects Initial Implementation
    Objects' data and public accessor to load the JSON DB set.

---
+++ YGO Card Collector 5/MasterCard.cs
@@ -0,0 +1,93 @@
+﻿//Joel Campos
+//1/27/2024
+//Master Card/Set Card Object Classes
+
+using System;
+using System.Collections.Generic;
+
+namespace YGO_Card_Collector_5
+{
+    public class MasterCard: IComparable<MasterCard>
+    {
+        #region Constructors
+        public MasterCard() { }
+        #endregion
+
+        #region Public Accessors
+        public string Name { get { return _Name; } set { _Name = value; } }
+        public int ID { get { return _ID; } set { _ID = value; } }
+        public string Attribute { get { return _Attribute; } set { _Attribute = value; } }
+        public string Type { get { return _Type; } set { _Type = value; } }
+        public string LevelRankLink { get { return _LevelRankLink; } set { _LevelRankLink = value; } }
+        public string Attack { get { return _Attack; } set { _Attack = value; } }
+        public string Defense { get { return _Defense; } set { _Defense = value; } }
+        public string Pendulum { get { return _Pendulum; } set { _Pendulum = value; } }
+        public string ProdeckURL { get { return _ProdeckURL; } set { _ProdeckURL = value; } }
+        public string KonamiURL { get { return _KonamiURL; } set { _KonamiURL = value; } }
+        public bool Obtained { get { return _Obtained; } set { _Obtained = value; } }
+        public List<SetCard> SetCards { get { return _SetCards; } }
+        #endregion
+
+        #region Internal Data
+        private int _ID = -1;
+        private string _Name;
+        private string _Attribute;
+        private string _Type;
+        private string _LevelRankLink;
+        private string _Attack;
+        private string _Defense;
+        private string _Pendulum;
+        private string _KonamiURL = "Missing";
+        private string _ProdeckURL = "Missing";
+        private bool _Obtained = false;
+        private List<SetCard> _SetCards = new List<SetCard>();
+        #endregion
+
+        #region IComparable Methods
+        public int CompareTo(MasterCard other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+        public class SortByName : IComparer<MasterCard>
+        {
+            public int Compare(MasterCard c1, MasterCard c2)
+            {
+                string card1tag = c1.Name;
+                string card2tag = c2.Name;
+                return String.Compare(card1tag, card2tag);
+            }
+        }
+        #endregion
+    }
+
+    public class SetCard
+    {
+        #region Constructors
+        public SetCard() 
+        { 
+        }
+        #endregion
+
+        #region Public Accessors
+        public string ReleaseDate { get { return _ReleaseDate; } set { _ReleaseDate = value; } }
+        public string Code { get { return _Code; } set { _Code = value; } }
+        public string Name { get { return _Name; } set { _Name = value; } }
+        public string Rarity { get { return _Rarity; } set { _Rarity = value; } }
+        public string MarketPrice { get { return _MarketPrice; } set { _MarketPrice = value; } }
+        public string MediamPrice { get { return _MediamPrice; } set { _MediamPrice = value; } }
+        public bool Obtained { get { return _Obtained; } set { _Obtained = value; } }
+        public string TCGPlayerURL { get { return _TCGPlayerURL; } set { _TCGPlayerURL = value; } }
+        #endregion
+
+        #region Internal Data
+        private string _ReleaseDate = "00/00/0000";
+        private string _Code = "XXXX-EN000";
+        private string _Name = "Invalid";
+        private string _Rarity = "Unknown";
+        private string _MarketPrice = "$0.00";
+        private string _MediamPrice = "$0.00";
+        private bool _Obtained = false;
+        private string _TCGPlayerURL = "Missing";
+        #endregion
+    }
+}
