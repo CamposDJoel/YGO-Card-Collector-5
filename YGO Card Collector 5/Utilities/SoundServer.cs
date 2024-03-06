607af5a920ebdd69d9f7d114a2d018eab2be702b
CamposDJoel <140183065+CamposDJoel@users.noreply.github.com>
2024-03-06 11:17:32 -0500

    Collector: Music now plays from a playlist
    The collector form now has a "Playlist" of songs to play. This list is shuffle the first time a collector form is open and plays the first song on the list. From there, once a song finish playing it will play the next song on the list until the playlist is completed. Once that happens the playlist shuffles again and the whole process repeats.
    
    A new song added: "LibraryMenu"

--- YGO Card Collector 5/Utilities/SoundServer.cs
+++ YGO Card Collector 5/Utilities/SoundServer.cs
@@ -2,7 +2,9 @@
 //2/19/2024
 //Sound Server Class
 
+using Medallion;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows.Media;
 
@@ -26,14 +28,31 @@ namespace YGO_Card_Collector_5
         {
             if (SettingsData.MusicOn)
             {
-                string song = "tmp";
-                Random rnd = new Random();
-                int index = rnd.Next(0, 2);
-                song = ((CollectorSong)index).ToString();
+                //Shuffle the playlist if this is the very first time running this function
+                if(CollectorPaylistIndex == -1)
+                {
+                    SongsPlaylist.Shuffle();
+                }
+
+                //Move the playlist iterator 1 up or ressetit back to the beginning
+                if(CollectorPaylistIndex == SongsPlaylist.Length -1)
+                {
+                    CollectorPaylistIndex = 0;
+                    //Shuffle the playlist
+                    SongsPlaylist.Shuffle();
+                }
+                else
+                {
+                    CollectorPaylistIndex++;
+                }
+
+                //Set the song to play
+                string song = SongsPlaylist[CollectorPaylistIndex].ToString();               
                 string filepath = "\\Music\\" + song + ".m4a";
                 CurrentBackGroundPlay.Open(new Uri(Directory.GetCurrentDirectory() + filepath));
+
                 //This event will loop the song... once the song ends it plays again
-                CurrentBackGroundPlay.MediaEnded += new EventHandler(Media_Ended);
+                CurrentBackGroundPlay.MediaEnded += new EventHandler(CollectorMedia_Ended);
                 CurrentBackGroundPlay.Play();
                 CurrentBackGroundPlay.Volume = 0.3;
             }
@@ -58,9 +77,21 @@ namespace YGO_Card_Collector_5
             CurrentBackGroundPlay.Position = TimeSpan.Zero;
             CurrentBackGroundPlay.Play();
         }
+        private static void CollectorMedia_Ended(object sender, EventArgs e)
+        {
+            PlayRNDBackgroundMusic();
+        }
 
         private static MediaPlayer CurrentBackGroundPlay = new MediaPlayer();
         private static MediaPlayer Effect = new MediaPlayer();
+        private static int CollectorPaylistIndex = -1;
+        private static CollectorSong[] SongsPlaylist = new CollectorSong[]
+        {
+            CollectorSong.DeckBuildMenu,
+            CollectorSong.FreeDuel,
+            CollectorSong.FreeDuelMenu,
+            CollectorSong.LibraryMenu
+        };
     }
 
     public enum Song
@@ -75,6 +106,8 @@ namespace YGO_Card_Collector_5
     {
         DeckBuildMenu,
         FreeDuel,
+        FreeDuelMenu,
+        LibraryMenu
     }
     public enum SoundEffect
     {
